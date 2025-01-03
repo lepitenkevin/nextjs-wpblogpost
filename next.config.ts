0a75668f6590298cd54b1a3c858e774c4c85d6e8
Kevin Victor Lepiten <lepitenkevin@gmail.com>
2025-01-04 04:00:15 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  /*output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
-  
+  */
 };
 
 export default nextConfig;
